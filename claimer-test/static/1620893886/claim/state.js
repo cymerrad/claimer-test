ba3f0cb7a19834e43ae5788cd466559faeb529f5
Radosław Cymer <cymerrad@gmail.com>
2021-05-13 10:52:41 +0200

    .

---
+++ claimer-test/static/1620893886/claim/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620893886",layout:"default",error:a,serverRendered:true,routePath:"\u002Fclaim",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4079d0ec"}}}}(null));
